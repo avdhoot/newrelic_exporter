822ea850ac144651f2422b431fcec39eac88c6eb
Vivek Jain <vivek@ridecell.com>
2023-07-14 17:42:21 +0530

    try exposing application deployments as metrics

--- newrelic_exporter.py
+++ newrelic_exporter.py
@@ -3,7 +3,9 @@
 import time
 import requests
 import click
+import json
 
+from datetime import datetime, timedelta
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
@@ -11,10 +13,11 @@ from prometheus_client.core import GaugeMetricFamily, REGISTRY
 class NewrelicCollector(object):
   def __init__(self, apikey):
     self.api_base_url = 'https://api.newrelic.com/'
+    self.graphql_base_url = "https://api.newrelic.com/graphql"
     self.api_key = apikey
 
   def collect(self):
-
+    print("Collecting\n")
     headers = {'X-Api-Key': self.api_key  }
     resp  = requests.get(self.api_base_url + 'v2/applications.json', headers=headers)
 
@@ -35,6 +38,48 @@ class NewrelicCollector(object):
             pass
       yield metrics[metric]
 
+    # Get all entity Guids
+    headers = {'API-Key': self.api_key}
+    resp  = requests.post(url=self.graphql_base_url, headers=headers, data="{actor {entitySearch(queryBuilder: {domain: APM}) {results {entities {name guid}}}}}")
+
+    if resp.json().get("errors"):
+      print("Error getting newrelic entities:", resp.json())
+      return
+    
+    guidDict = {}
+    for entity in resp.json().get("data").get("actor").get("entitySearch").get("results").get("entities"):
+      guidDict[entity["guid"]] = entity["name"]
+    
+    # We can pass only 25 Guids in one request, so dividing all guids in chunks of 25,
+    # and fetching deployments for those 25 guids
+    # Get list of deployment which happened in last 1 hour
+    t = datetime.today()-timedelta(hours=1)
+    deploymentArray = []
+    guids = list(guidDict.keys())
+    chunk_size = 25
+    for i in range(0, len(guids), chunk_size):
+    
+      resp  = requests.post(url=self.graphql_base_url, headers=headers, data='{{actor {{entities(guids: {0}) {{deploymentSearch(filter: {{timeWindow: {{startTime: {1} }}}}) {{results {{version timestamp entityGuid}}}}}}}}}}'.format(json.dumps(guids[i:i+chunk_size]),round(t.timestamp()*1000)))
+      if resp.json().get("errors"):
+        print("Error getting newrelic deployments:", resp.json())
+        return
+
+      for entity in resp.json().get("data").get("actor").get("entities"):
+        for deployments in entity.get("deploymentSearch").get("results"):
+          deploymentArray.append({
+            "name": guidDict.get(deployments.get("entityGuid")),
+            "timestamp": deployments.get("timestamp"),
+            "version": deployments.get("version")
+          })
+    
+    print(deploymentArray)
+    # Expose deployment metric
+    deploymentMetric = GaugeMetricFamily('newrelic_application_deployment',' newrelic application deployment', labels=["appname", "version"])
+    for deployment in deploymentArray:
+      # Passing timestamp as seconds because prometheus client automatically converts timestamp to milliseconds
+      deploymentMetric.add_metric([deployment['name'], deployment['version']], 1, int(deployment['timestamp']/1000))
+    yield deploymentMetric
+
 @click.command()
 @click.option('--api-key', '-a', envvar='APIKEY', help='API key for newrelic', required=True)
 def main(api_key):
@@ -42,6 +87,7 @@ def main(api_key):
   start_http_server(9126)
   while True: 
     time.sleep(1)
+    
 if __name__ == "__main__":
   main()
 
